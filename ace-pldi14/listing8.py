a8c0019de58f2a69f2c4b44bc3f829e4e7b85e89
Cyrus Omar <cyology@gmail.com>
2013-11-14 21:41:38 -0500

    pldi14 section 2 first draft done (whew)

--- ace-pldi14/listing8.py
+++ ace-pldi14/listing8.py
@@ -1,12 +1,12 @@
 import listing1, listing2, examples.clx as clx, numpy
 
-clx.default_ctx = clx.Context.for_device(0, 0)
+clx.opencl.ctx = clx.Context.for_device(0, 0)
 
 input = numpy.ones((1024,))
 d_input = clx.to_device(input)
 d_output = clx.alloc(like=input)
 
-listing1.map(d_input, d_output, listing2.thresh_dbl, 
-  global_size=d_in.shape, local_size=(128,))
+listing1.map(d_input, d_output, listing2.negate, 
+  global_size=d_in.shape)
 
-assert (cl.from_device(d_out) == input * 2).all()
+assert (cl.from_device(d_out) == input * -1).all()
