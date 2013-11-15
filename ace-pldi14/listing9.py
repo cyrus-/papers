fb3c87e7cfab420d9791d7b41c6c237535ab8721
Cyrus Omar <cyology@gmail.com>
2013-11-15 10:22:24 -0500

    pldi14 sectionized sec 4 roughly

--- ace-pldi14/listing9.py
+++ ace-pldi14/listing9.py
@@ -1,25 +1,36 @@
 import ace
 
-class PtrType(ace.Type):
-  def __init__(self, target_type, addr_space):
-    self.target_type = target_type
+class Ptr(ace.ActiveType):
+  def __init__(self, addr_space, target_t):
     self.addr_space = addr_space
+    self.target_t = target_type
         
-  def resolve_Subscript(self, context, node):
-    slice_type = context.resolve(node.slice)
-    if isinstance(slice_type, IntegerType):
-      return self.target_type
+  def type_Subscript(self, context, node):
+    slice_t = context.type(node.slice)
+    if isinstance(slice_t, Integer):
+      return self.target_t
     else:
-      raise TypeError('<error message>', node)
+      raise ace.TypeError('<error message>', node)
        
   def translate_Subscript(self, context, node):
-    value = context.translate(node.value)
-    slice = context.translate(node.slice)
-    return ace.copy_node(node, 
-      value=value, slice=slice,    
-      code=value.code + '[' + slice.code + ']')      
-  
-  # ...
-  
-def gptr(target_type):
-    return PtrType(target_type, "__global")
+    value_x = context.translate(node.value)
+    slice_x = context.translate(node.slice)
+    return context.target.Subscript(value_x, slice_x)
+
+  def type_BinOp(self, context, node):
+    if node.op == ast.Add:
+      right_t = context.type(node.right)
+      if isinstance(right_t, Integer):
+        return self
+    elif node.op == ast.Sub:
+      right_t = context.type(node.right)
+      if (isinstance(right_t, Ptr) and 
+          right_t.addr_space == self.addr_space):
+        return ptrdiff_t
+    else: raise ace.TypeError('<error message>', node)
+
+  def translate_BinOp(self, context, node):
+    left_x = context.translate(node.left)
+    right_x = context.translate(node.right)
+    return context.target.BinOp(left_x, 
+      node.operator, right_x)
