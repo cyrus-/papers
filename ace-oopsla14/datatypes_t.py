8cdf90c13f03bce3ff3e011dfb81b26f0e65584e
Cyrus Omar <cyology@gmail.com>
2014-03-26 08:18:17 -0400

    OOPSLA14 final

--- ace-oopsla14/datatypes_t.py
+++ ace-oopsla14/datatypes_t.py
@@ -1,25 +1,24 @@
-import ace, ace.python as py, examples.fp as fp
-
-Tree = lambda name, a: fp.Datatype(name, 
+tree = lambda name, a: fp.data(name, 
   lambda tree: {
     'Empty': fp.unit,
     'Leaf': a,
     'Node': fp.tuple(tree, tree)
   })
 
-DT = Tree('DT', py.dyn)
+dyntree = tree(dyn)
 
-@ace.fn(py.Base(trailing_return=True))[[DT]]
+@py
 def depth_gt_2(x):
-  x.case({
+  {x : dyntree}
+  return x.case({
     DT.Node(DT.Node(_), _): True,
     DT.Node(_, DT.Node(_)): True,
     _: False
   })
 
-@ace.fn(py.Base(main=True))[[]]
-def __main__():
-  my_lil_tree = DT.Node(DT.Empty, DT.Empty)
-  my_big_tree = DT.Node(my_lil_tree, my_lil_tree)
+@py
+def __toplevel__():
+  my_lil_tree = dyntree.Node(dyntree.Empty, dyntree.Empty)
+  my_big_tree = dyntree.Node(my_lil_tree, my_lil_tree)
   assert not depth_gt_2(my_lil_tree)
   assert depth_gt_2(my_big_tree)
